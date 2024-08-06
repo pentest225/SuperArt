0cae5e06f7c7ff026657b23b1be2d12525a4ee0d
pentest225 <47216215+pentest225@users.noreply.github.com>
2024-08-06 01:57:46 +0000

    Integration du premier template

--- SuperArt/settings.py
+++ SuperArt/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "users.apps.UsersConfig",
-    "services.apps.ServicesConfig"
+    "services.apps.ServicesConfig",
+    "frontend.apps.FrontendConfig"
 ]
 
 MIDDLEWARE = [
